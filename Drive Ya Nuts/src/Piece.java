b536483fc5889cdfc6e8150f034633e1be21bd7d
Marjorie Carlson <marjoriecarlson@gmail.com>
2014-01-02 16:50:43 -0700

    Drive Ya Nuts
    A program to solve “Drive Ya Nuts.”

---
+++ Drive Ya Nuts/src/Piece.java
@@ -0,0 +1,49 @@
+/* Marjorie Carlson, Jan. 2 2014
+ * 
+ * A helper class for the Drive Ya Nuts puzzle solver. Instantiates a puzzle piece and gives
+ * methods to find the relevant values of the piece.
+ */
+
+import java.util.Arrays;
+
+public class Piece {
+	private int[] sides;
+
+	// a piece is constructed by an array of 6 ints, which are the numbers in
+	// clockwise order starting from the 1. Thus the first value is always 1.
+	// For example, the piece with the numbers 1-6 in clockwise order would
+	// be constructed with the argument [1, 2, 3, 4, 5, 6].
+	public Piece(int[] inputSides) {
+		sides = new int[6];
+		System.arraycopy(inputSides, 0, sides, 0, inputSides.length);
+	}
+	
+	public int findValueByPosition(int clockwiseDistanceFromOne) {
+		return sides[clockwiseDistanceFromOne];
+	}
+	
+	public int findPositionByValue(int value) {
+		for (int i = 0; i < 6; i++) {
+			if (sides[i] == value) {
+				return i;
+			}
+		}
+		return -1; // if the value doesn't exist...
+	}
+	
+	public int findClockwiseNeighborOfValue(int value) {
+		int positionOfValue = findPositionByValue(value);
+		int positionOfClockwiseNeighbor = (positionOfValue+1)%6;
+		return findValueByPosition(positionOfClockwiseNeighbor);
+	}
+	
+	public int findCounterClockwiseNeighborOfValue(int value) {
+		int positionOfValue = findPositionByValue(value);
+		int positionOfCounterClockwiseNeighbor = (positionOfValue+5)%6;
+		return findValueByPosition(positionOfCounterClockwiseNeighbor);
+	}
+	
+	public String toString() {
+		return Arrays.toString(sides);
+	}	
+}
---
+++ Drive Ya Nuts/src/Puzzle.java
@@ -0,0 +1,91 @@
+/* Marjorie Carlson, Jan. 2 2014.
+ * 
+ * A program to solve "Drive Ya Nuts," a silly game by Milton Bradley. Seven hexagonal pieces must be
+ * placed, with one piece in the middle and the other six surrounding it. Each piece's sides are
+ * numbered with the numerals 1-6, but with the numbers in varying orders. The pieces must be placed so
+ * that all contiguous sides match in number, e.g., one piece's side labeled "1" adjoins its neighbor's
+ * side labeled "1." The (solved) puzzle is viewable here:
+ * http://www.hasbro.com/common/instruct/DriveYaNuts.PDF.
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Puzzle {
+	public static Piece[] pieces = new Piece[]{
+		new Piece(new int[] {1, 2, 3, 4, 5, 6}),
+		new Piece(new int[] {1, 6, 5, 4, 3, 2}),
+		new Piece(new int[] {1, 4, 3, 6, 5, 2}),
+		new Piece(new int[] {1, 6, 2, 4, 5, 3}),
+		new Piece(new int[] {1, 4, 6, 2, 3, 5}),
+		new Piece(new int[] {1, 6, 5, 3, 2, 4}),
+		new Piece(new int[] {1, 6, 4, 2, 5, 3})};
+	
+	// returns an array of the pieces that could legally be placed next, given an arraylist
+	// of currently placed pieces. Pieces are placed in the following order: the zeroth element is the
+	// hub; the next piece is placed so that it lines up with the "1" of the hub pieces, and all following
+	// pieces are placed in clockwise order from that piece.
+	public ArrayList<Piece> findPotentialPieces(ArrayList<Piece> piecesSoFar) {
+		int numberOfPieces = piecesSoFar.size();
+		ArrayList<Piece> potentialPieces = new ArrayList<Piece>();
+		
+		switch (numberOfPieces) {
+		case 0: // if no pieces are placed, all pieces are potential
+			for (int i = 0; i < 7; i++) {
+				potentialPieces.add(pieces[i]);
+			}
+			break;
+			
+		case 1: // if only the hub is placed, all other pieces are potential
+			for (int i = 0; i < 7; i++) {
+				if (!piecesSoFar.contains(pieces[i])) {
+					potentialPieces.add(pieces[i]);
+				}	
+			}
+			break;
+		
+		default: // find pieces that match both the next open spot on the hub and the open spot on
+				 // the previously placed piece. If this is the seventh and final piece, the piece
+			     // must also match the other open spot on the first edge piece placed.
+			  	 // if this is the last piece to be placed, also the first piece's value.
+			int hubValue = piecesSoFar.get(0).findValueByPosition(numberOfPieces - 1);
+			int hubValueOfPreviousPiece = piecesSoFar.get(0).findValueByPosition(numberOfPieces - 2);
+			int prevPieceValue = piecesSoFar.get(numberOfPieces - 1).findCounterClockwiseNeighborOfValue(hubValueOfPreviousPiece);
+			int firstPieceValue = piecesSoFar.get(1).findClockwiseNeighborOfValue(1);
+						
+			for (int i = 0; i < 7; i++) {
+				if (!piecesSoFar.contains(pieces[i]) &&
+					pieces[i].findClockwiseNeighborOfValue(hubValue) == prevPieceValue &&
+					(numberOfPieces < 6 || pieces[i].findCounterClockwiseNeighborOfValue(hubValue) == firstPieceValue)) {
+					potentialPieces.add(pieces[i]);
+				}
+			}
+		}
+		return potentialPieces;
+	}
+
+	
+	// recursively solves the puzzle. Takes an arraylist of the pieces already placed. If all are placed,
+	// it outputs the solution; otherwise, it iterates through each potential piece that COULD legally be
+	// placed next, add it to pieces already placed, and recurse.
+	public void placePieces(ArrayList<Piece> piecesSoFar){
+		if (piecesSoFar.size() == 7) {
+			System.out.println(piecesSoFar.toString());
+		}
+		else {
+			ArrayList<Piece> potentialPieces = findPotentialPieces(piecesSoFar);
+			for (Piece p : potentialPieces) {
+					ArrayList<Piece> clone = (ArrayList<Piece>) piecesSoFar.clone();
+					clone.add(p);
+					placePieces(clone);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Puzzle puzzle = new Puzzle();
+		puzzle.placePieces(new ArrayList<Piece>());
+		
+		// Output: [[1, 6, 2, 4, 5, 3], [1, 4, 6, 2, 3, 5], [1, 6, 5, 3, 2, 4],
+		// [1, 4, 3, 6, 5, 2], [1, 2, 3, 4, 5, 6], [1, 6, 4, 2, 5, 3], [1, 6, 5, 4, 3, 2]]
+	}
+}
